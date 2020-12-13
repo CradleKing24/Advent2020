c894a2164cb371ccbc0e3a527ec39e864e8c1e40
Jaron <jaronbturner@gmail.com>
2020-12-12 20:00:49 -0600

    p2 init

--- src/twenty_twenty/day4/day4.py
+++ src/twenty_twenty/day4/day4.py
@@ -1,5 +1,6 @@
 from src import custom_read_file as reader
 import re
+from src.twenty_twenty.day4 import validator as valid
 
 
 def get_day_input():
@@ -64,19 +65,13 @@ def run_part_1(data):
     return valid_ports
 
 
-def check_years(value, low, high):
-    if_value_passed = False
-    if low <= value <= high:
-        if_value_passed = True
-    return if_value_passed
-
-
 def check_regex(regex, value):
     if_value_passed = False
     pattern = re.compile(regex)
     if pattern.match(value):
         if_value_passed = True
     return if_value_passed
+
 
 def run_part_2(data):
     clean_input = sanitize_data(data)
@@ -89,35 +84,32 @@ def run_part_2(data):
         for each_attribute in port:
             if each_attribute.find("byr") > -1:
                 attribute_val = each_attribute.split(":")
-                if check_years(int(attribute_val[1]), 1920, 2002):
+                if valid.check_range(int(attribute_val[1]), 1920, 2002):
                     criteria_count += 1
             if each_attribute.find("iyr") > -1:
                 attribute_val = each_attribute.split(":")
-                if check_years(int(attribute_val[1]), 2010, 2020):
+                if valid.check_range(int(attribute_val[1]), 2010, 2020):
                     criteria_count += 1
             if each_attribute.find("eyr") > -1:
                 attribute_val = each_attribute.split(":")
-                if check_years(int(attribute_val[1]), 2020, 2030):
+                if valid.check_range(int(attribute_val[1]), 2020, 2030):
                     criteria_count += 1
             if each_attribute.find("hgt") > -1:
                 attribute_val = each_attribute.split(":")
-                if attribute_val[1].find("cm") > -1:
-                    if check_regex("^[0-9]{3}cm$", attribute_val[1]):
-                        criteria_count += 1
-                elif attribute_val[1].find("in") > -1:
-                    if check_regex("^[0-9]{2}in$", attribute_val[1]):
-                        criteria_count += 1
-
-                criteria_count += 1
+                if valid.check_height(attribute_val[1]):
+                    criteria_count += 1
             if each_attribute.find("hcl") > -1:
                 attribute_val = each_attribute.split(":")
-                pattern = re.compile("^#[a-f0-9]{6}$")
-                if pattern.match(attribute_val[1]):
+                if valid.check_hair_color(attribute_val[1]):
                     criteria_count += 1
             if each_attribute.find("ecl") > -1:
-                criteria_count += 1
+                attribute_val = each_attribute.split(":")
+                if valid.check_eye_color(attribute_val):
+                    criteria_count += 1
             if each_attribute.find("pid") > -1:
-                criteria_count += 1
+                attribute_val = each_attribute.split(":")
+                if valid.check_pass_id(attribute_val):
+                    criteria_count += 1
         if criteria_count == 7:
             valid_ports += 1
     return valid_ports
--- tests/twenty_twenty/test_day4.py
+++ tests/twenty_twenty/test_day4.py
@@ -11,6 +11,12 @@ class TestDay1(TestCase):
         answer = day4.run_part_1(test_input)
         self.assertEqual(2, answer)
 
+    def test_run_part_2(self):
+        test_input = reader.return_data_day4("./day4_p2.csv")
+
+        answer = day4.run_part_2(test_input)
+        self.assertEqual(4, answer)
+
     def test_loop_input_part_2(self):
         test_input = ["..##.......",
                       "#...#...#..",
